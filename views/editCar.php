8a4f24df8aa6d4399f74ea5b7f76cdbc6b584293
Shikhar Mall <shmall.21.2020@gmail.com>
2024-02-03 19:33:15 +0530

    final added

--- views/editCar.php
+++ views/editCar.php
@@ -40,6 +40,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental Car | Add Car</title>
 
+    <style>
+        /* Style for the modal container */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(0,0,0);
+            background-color: rgba(0,0,0,0.4);
+            padding-top: 60px;
+        }
+        /* Style for the modal content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 5% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+        /* Style for the close button */
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        .image-preview {
+            margin-top: 10px;
+            max-width: 100%;
+            height: auto;
+            display: none;
+        }
+    </style>
+
   <!-- 
     - favicon
   -->
@@ -163,7 +208,6 @@
         var input = document.getElementById('imageInput');
         var preview = document.getElementById('imagePreview');
         var previewHeading = document.getElementById('previewHeading');
-        var FormBox = document.getElementsByClassName('Form-box')[0];
 
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -173,7 +217,6 @@
                 input.style.display = 'none';
                 previewHeading.style.display = 'none';
                 preview.style.display = 'block';
-                FormBox.style.height = "1300px";
             };
 
             reader.readAsDataURL(input.files[0]);
